288e5764be510c7ce65e927ffed63e55de60083e
nurfadlisetiaji21-design <nurfadlisetiaji21@gmail.com>
2026-06-18 18:31:59 +0700

    T

--- marketplace.cpp
+++ marketplace.cpp
@@ -63,4 +63,49 @@ int main() {
 
     } while (pilihan != 2);
     return 0;
+
+    string barang, asal, tujuan;
+    string kurir[3] = {"JNE", "J&T", "SiCepat"};
+    int pilihan;
+    int estimasi;
+
+    cout << "SISTEM PENGIRIMAN BARANG" << endl;
+
+    cout << "Masukkan nama barang : ";
+    getline(cin, barang);
+
+    cout << "Masukkan kota asal : ";
+    getline(cin, asal);
+
+    cout << "Masukkan kota tujuan : ";
+    getline(cin, tujuan);
+
+    cout << "\nPilih Kurir :" << endl;
+
+    for (int i = 0; i < 3; i++) {
+        cout << i + 1 << ". " << kurir[i] << endl;
+    }
+
+    cout << "Pilihan : ";
+    cin >> pilihan;
+
+    if (pilihan == 1) {
+        estimasi = 3;
+    }
+    else if (pilihan == 2) {
+        estimasi = 2;
+    }
+    else if (pilihan == 3) {
+        estimasi = 1;
+    }
+    else {
+        cout << "Pilihan tidak valid!" << endl;
+    }
+
+    cout << "\n=== DATA PENGIRIMAN ===" << endl;
+    cout << "Barang          : " << barang << endl;
+    cout << "Asal            : " << asal << endl;
+    cout << "Tujuan          : " << tujuan << endl;
+    cout << "Kurir           : " << kurir[pilihan - 1] << endl;
+    cout << "Estimasi Sampai : " << estimasi << " hari" << endl;
 }
